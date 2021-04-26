77a97a50b977d5d35d3fac4093c840a41c3a36c2
Sebastian Guerra <spguerraespinoza@gmail.com>
2021-04-26 00:59:38 -0400

    mas opciones

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ if(fs.existsSync(SESSION_FILE_PATH)) {
 
 // Use the saved values
 const client = new Client({
-	session: sessionData,
-	puppeteer: {
-		executablePath:'/usr/bin/google-chrome-stable'
-	}
+	session: sessionData
 });
 
 client.on('authenticated', (session) => {
@@ -36,12 +33,74 @@ client.on('ready', () => {
 });
 
 client.on('message', async (msg) => {
-	if(msg.body === "!ping") {
-		msg.reply('pong')
-	}else if(msg.body.includes("!ping")){
-		msg.reply('pong')
+	const user = await msg.getContact()
+	console.log(user)
+	console.log(user.id)
+	console.log(user.id.user)
+	const texto = msg.body.toLowerCase()
+	if(texto.includes('linux') ) {
+		msg.reply("I use Arch btw")
 	}
-	let user = await msg.getContact();
+
+	let matchesPing = texto.match(/!ping/g)
+	let matchesPong = texto.match(/!pong/g)
+	if(matchesPing )
+	if(matchesPing){
+		cantidad = matchesPing.length
+		if(cantidad > 1){
+			let res = ''
+			for(let i = 0; i <= cantidad; i++) {
+				res += 'pong '
+			}
+			msg.reply(res+'gane;)')
+		} else {
+			msg.reply('pong')
+		}
+	}
+	
+	if(matchesPong){
+		cantidad = matchesPong.length
+		if(cantidad > 1){
+			let res = ''
+			for(let i = 0; i <= cantidad; i++) {
+				res += 'ping '
+			}
+			msg.reply(res+'gane;)')
+		} else {
+			msg.reply('ping')
+		}
+	}
+
+
+	if(texto.includes('!kill') || texto.includes('/kill')) {
+		const deathMessages = [
+			"was pricked to death", 
+			"drowned",
+			"experienced kinetic energy",
+			"blew up",
+			"hit the ground too hard",
+			"fell from a high place",
+			"fell off a ladder",
+			"fell off some vines",
+			"fell off some weeping vines",
+			"fell off scaffolding",
+			"fell while climbing",
+			"was squashed b a falling anvil",
+			"was squashed by a falling block",
+			"went up in flames",
+			"burned to death",
+			"tried to swim in lava",
+			"was struck by lightning",
+			"discovered the floor was lava",
+			"was killed by magic",
+		]
+
+		chat = await msg.getChat()
+		chat.sendMessage(`@${user.id.user} ${deathMessages[parseInt(Math.random()*deathMessages.length)]}`, {
+			mentions: [user]
+		})
+	}
+
 	console.log(user.name || user.pushname, ": ", msg.body);
 });
 
